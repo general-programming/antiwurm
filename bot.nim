ac11036713ae48224a92da47791172005ceb54bf
Henry <omgredstone@gmail.com>
2018-03-03 15:36:32 +0000

    Vote via named pipe

--- bot.nim
+++ bot.nim
@@ -1,16 +1,25 @@
-import irc, asyncnet, asyncdispatch, parsecfg, tables, strutils
+import irc, asyncnet, asyncdispatch, parsecfg, tables, strutils, asyncfile, os
 
 type
     IrcConfig = tuple[host: string, user: string, password: string, channels: seq[string]]
-    CommandCallable = proc (c: AsyncIrc, ev: IrcEvent, m: string): Future[void]
+    CommandCallable = proc (c: AsyncIrc, ev: IrcEvent, a: seq[string]): Future[void]
 
 var commands = initTable[string, CommandCallable](16)
+let pipe = openAsync("wurm_comms", fmReadWrite)
 
-commands["ping"] = proc (client: AsyncIrc, ev: IrcEvent, msg: string) {.async.} =
+commands["ping"] = proc (client: AsyncIrc, ev: IrcEvent, args: seq[string]) {.async.} =
     await client.privmsg(ev.origin, "Pong!")
 
-commands["pong"] = proc (client: AsyncIrc, ev: IrcEvent, msg: string) {.async.} =
+commands["pong"] = proc (client: AsyncIrc, ev: IrcEvent, args: seq[string]) {.async.} =
     await client.privmsg(ev.origin, ev.nick & " likes cute asian boys.")
+
+commands["vote"] = proc (client: AsyncIrc, ev: IrcEvent, args: seq[string]) {.async.} =
+    if args.len < 1:
+        await client.privmsg(ev.origin, ev.nick & ": You need to specify a number to vote!")
+        return
+
+    let parts = ["vote", ev.nick, ev.origin, args[0]]
+    await pipe.write(parts.join(":") & "\n")
 
 proc handleMessage(client: AsyncIrc, ev: IrcEvent, msg: string) {.async.} =
     if msg.startsWith('?'):
@@ -19,7 +28,7 @@ proc handleMessage(client: AsyncIrc, ev: IrcEvent, msg: string) {.async.} =
         args.delete(0)
 
         if commands.hasKey(cmd):
-            await commands[cmd](client, ev, msg)
+            await commands[cmd](client, ev, args)
 
 proc handleEvent(client: AsyncIrc, ev: IrcEvent) {.async.} =
     if ev.typ == EvMsg and ev.cmd == MPrivMsg:
@@ -31,6 +40,7 @@ proc connect(config: IrcConfig) =
     echo "Twitch Plays starting..."
     asyncCheck client.run()
     runForever()
+    pipe.close()
 
 let config = loadConfig("config.ini")
 
